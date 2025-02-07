7d16d59274b1cfb6bb968eb29bd05b3373907a85
Jaideep Kumar <jaideep.kumar@lenskart.in>
2025-02-07 19:25:51 +0530

    Initial Commit

---
+++ JaideepCurrencyConvertorTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JaideepCurrencyConvertorTask
+//
+//  Created by Jaideep on 07/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JaideepCurrencyConvertorTask/JaideepCurrencyConvertorTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  JaideepCurrencyConvertorTaskApp.swift
+//  JaideepCurrencyConvertorTask
+//
+//  Created by Jaideep on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct JaideepCurrencyConvertorTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
